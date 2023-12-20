f79cbd5930cdd2a27b3a812610127371bd4cd90b
Br4z <67658575+Br4z@users.noreply.github.com>
2023-12-20 19:06:40 +0000

    Edit 21 C++ exercise path

--- C++/21-Grade_School/grade_school.cpp
+++ C++/21-Grade_School/grade_school.cpp
@@ -5,8 +5,26 @@
 #include "grade_school.h"
 
 
+// void displace_names_vector(std::vector <std::string> &names, int position) { // End to position
+// 	for (int i = names.size() - 1; i > position; i--) {
+// 		names[i] = names[i - 1];
+// 	}
+// }
+
+void displace_names_vector(std::vector <std::string> &names, int position) { // Position to end
+	if (names.size() > 1) {
+		std::string to_replace = names[position];
+
+		for (int i = position + 1; i < (int) names.size(); i++) {
+			std::string temp = names[i];
+			names[i] = to_replace;
+			to_replace = temp;
+		}
+	}
+}
+
 void grade_school::print_names(std::vector <std::string> names) {
-	for (long unsigned int i = 0; i < names.size(); i++) {
+	for (int i = 0; i < (int) names.size(); i++) {
 		std::cout << names[i] << std::endl;
 	}
 }
@@ -20,16 +38,18 @@ grade_school::school::~school() {
 }
 
 void grade_school::school::add(std::string name, int grade) {
-	std::vector <std::string>& names = school_roster[grade];
+	std::vector <std::string> &names = school_roster[grade];
 	names.resize(names.size() + 1);
 
 	// auto at = std::lower_bound(names.begin(), names.end(), name);
 	// names.insert(at, name);
 
-	for (long unsigned int i = 0; i < names.size(); i++) {
-		if (names[i] > name || names[i] == "") {
-			names.insert(names.begin() + i, name);
-			names.pop_back(); break; // Remove the last name (empty name)
+	for (int i = 0; i < (int) names.size(); i++) {
+		if (names[i] == "") {
+			names[i] = name; break;
+		} else if (names[i] > name) {
+			displace_names_vector(names, i);
+			names[i] = name; break;
 		}
 	}
 }
