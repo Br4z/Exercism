f1a6432b4083bca7d23d80eac7370686e146a680
Br4z <67658575+Br4z@users.noreply.github.com>
2024-01-21 19:01:18 +0000

    Add 54 adn 55 C++ track exercises

---
+++ C++/54-Matching_Brackets/matching_brackets.cpp
@@ -0,0 +1,92 @@
+// URL: https://exercism.org/tracks/cpp/exercises/matching-bracketsmatching_brackets DATE: 2024-01-20
+
+#include <stdexcept>
+#include <stack>
+
+#include "matching_brackets.h"
+
+
+/**
+ * Check if the given character is an opening character.
+ *
+ * @param character The character to check.
+ * @return True if the character is an opening bracket, brace,
+ * or parenthesis, false otherwise.
+ */
+bool is_opening_character(char &character) {
+	switch (character)	{
+		case '[':
+		case '{':
+		case '(':
+			return true;
+		default:
+			return false;
+	}
+}
+
+/**
+ * Check if the given character is a closing character.
+ *
+ * @param character The character to check.
+ * @return True if the character is a closing bracket, brace,
+ * or parenthesis, false otherwise.
+ */
+bool is_closing_character(char &character) {
+	switch (character) {
+		case ']':
+		case '}':
+		case ')':
+			return true;
+		default:
+			return false;
+	}
+}
+
+/**
+ * Get the corresponding closing character for a given opening character.
+ *
+ * @param opening_character The opening character to find the closing
+ * character for.
+ * @return The corresponding closing character.
+ * @throws std::domain_error if the input character is not a valid
+ * opening character.
+ */
+char opening_character_pair(char &opening_character) {
+	switch (opening_character) {
+		case '[':
+			return ']';
+			break;
+		case '{':
+			return '}';
+			break;
+		case '(':
+			return ')';
+			break;
+	}
+
+	throw std::domain_error("Invalid opening character");
+}
+
+/**
+ * Check if the brackets in the given string are matched and nested correctly.
+ *
+ * This function verifies that any and all pairs of brackets (round, square, and curly) in
+ * the given string are correctly matched and nested.
+ *
+ * @param text The input string containing brackets and other characters.
+ * @return True if all brackets are matched and nested correctly, false otherwise.
+ */
+bool matching_brackets::check(std::string text) {
+	std::stack <char> stack;
+
+	for (char &character : text)
+		if (is_opening_character(character))
+			stack.push(opening_character_pair(character));
+		else if (is_closing_character(character))
+			if (stack.empty() || stack.top() != character)
+				return false;
+			else
+				stack.pop();
+
+	return stack.empty();
+}
